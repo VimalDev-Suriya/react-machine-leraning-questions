dc92ef37575775052ba0f4f48bc4da42231424aa
VimalDev-Suriya <suriyadevaraj4@gmail.com>
2026-01-07 00:12:08 +0530

    adding the additional feature in toast

--- src/components/Toast/Toast.jsx
+++ src/components/Toast/Toast.jsx
@@ -1,8 +1,19 @@
+import { useEffect } from 'react';
 import { CLOSE } from './constants';
 import ToastIcon from './ToastIcon';
 
 const Toast = (props) => {
   const { id, message, isClosable = true, type, handleClick } = props;
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      handleClick(id);
+    }, 5000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
 
   return (
     <div className={`toast toast-${type}`}>
--- src/components/Toast/ToastContainer.jsx
+++ src/components/Toast/ToastContainer.jsx
@@ -1,13 +1,9 @@
-import { useContext } from 'react';
-import { ERROR, SUCCESS } from './constants';
+import { useContext, useEffect } from 'react';
 import Toast from './Toast';
 import { ToastContext } from './ToastContext';
 
 const ToastContainer = () => {
   const { toasts, removeToast } = useContext(ToastContext);
-
-  console.log('toasts', toasts);
-
   const position = 'top-right';
 
   const handleToastClose = (i) => {
@@ -16,22 +12,12 @@ const ToastContainer = () => {
 
   return (
     <div className={`toast-container toast-pos-${position}`}>
-      {/* <Toast
-        id={'test - 1'}
-        message="hello its is success"
-        type={SUCCESS}
-        isClosable={true}
-        handleClick={handleToastClose}
-      />
-      <Toast
-        id={'test - 2'}
-        message="hello its is failure"
-        type={ERROR}
-        isClosable={true}
-        handleClick={handleToastClose}
-      /> */}
-      {toasts.map((toast) => (
-        <Toast key={'id-toast-1'} {...toast} handleClick={handleToastClose} />
+      {toasts.map((toast, idx) => (
+        <Toast
+          key={`toast-id-${idx}-${toast.message}`}
+          {...toast}
+          handleClick={handleToastClose}
+        />
       ))}
     </div>
   );
